5e5a0e1a132eed2581c02f49202d21a6b8187efb
jane <jane.aziz@etu.univ-lyon1.fr>
2023-09-30 18:39:19 +0200

    modificiation port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
